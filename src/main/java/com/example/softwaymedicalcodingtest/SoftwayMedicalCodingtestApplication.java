90d529739d1989b5ce8a5c1f88c7f57db4750fa6
Abdallah <hameurlain.abdallah@gmail.com>
2023-10-21 14:04:11 +0200

    init

---
+++ src/main/java/com/example/softwaymedicalcodingtest/SoftwayMedicalCodingtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwaymedicalcodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwayMedicalCodingtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwayMedicalCodingtestApplication.class, args);
+	}
+
+}
